5c3ae9e64d214644b4a414c805270e8e7b53fece
Josh Yoo <josh.yoo@topazlabs.com>
2025-07-01 10:57:54 -0500

    conanfile changes for toolchain

--- build-scripts/deploy_conanfile.py
+++ build-scripts/deploy_conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.files import copy, collect_libs
+import os
 
 class conanRecipe(ConanFile):
     name = "topaz-ffmpeg"
@@ -24,13 +25,110 @@ class conanRecipe(ConanFile):
         self.info.requires["videoai"].minor_mode()
 
     def package(self):
+        # Copy header files from include folder or root
+        src_include = os.path.join(self.source_folder, "include")
+        if os.path.exists(src_include):
+            copy(
+                self,
+                "*",
+                src=src_include,
+                dst=os.path.join(self.package_folder, "include"),
+                keep_path=True,
+            )
+        else:
+            # Copy header files from root if no include folder
+            copy(
+                self,
+                "*.h",
+                src=self.source_folder,
+                dst=os.path.join(self.package_folder, "include"),
+                keep_path=True,
+            )
+        
+        # Copy libraries to lib folder (from any location in source)
         copy(
             self,
-            "*",
+            "*.lib",
             src=self.source_folder,
-            dst=self.package_folder,
-            keep_path=True,
+            dst=os.path.join(self.package_folder, "lib"),
+            keep_path=False,
         )
+        copy(
+            self,
+            "*.a",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.so*",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.dylib",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        
+        # Copy DLLs and executables to bin folder (from any location in source)
+        copy(
+            self,
+            "*.dll",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "bin"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.exe",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "bin"),
+            keep_path=False,
+        )
+        
+        # Copy pkg-config files
+        copy(
+            self,
+            "*.pc",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "lib", "pkgconfig"),
+            keep_path=False,
+        )
+
+    def package_info(self):
+        # Define individual components for each FFmpeg library
+        
+        # avutil - core utility library (base for others)
+        self.cpp_info.components["avutil"].libs = ["avutil"]
+        
+        # avcodec - codec library
+        self.cpp_info.components["avcodec"].libs = ["avcodec"]
+        self.cpp_info.components["avcodec"].requires = ["avutil"]
+        
+        # avformat - format library
+        self.cpp_info.components["avformat"].libs = ["avformat"]
+        self.cpp_info.components["avformat"].requires = ["avutil", "avcodec"]
+        
+        # avfilter - filter library
+        self.cpp_info.components["avfilter"].libs = ["avfilter"]
+        self.cpp_info.components["avfilter"].requires = ["avutil"]
+        
+        # avdevice - device library
+        self.cpp_info.components["avdevice"].libs = ["avdevice"]
+        self.cpp_info.components["avdevice"].requires = ["avutil", "avformat"]
+        
+        # swscale - scaling library
+        self.cpp_info.components["swscale"].libs = ["swscale"]
+        self.cpp_info.components["swscale"].requires = ["avutil"]
+        
+        # swresample - resampling library
+        self.cpp_info.components["swresample"].libs = ["swresample"]
+        self.cpp_info.components["swresample"].requires = ["avutil"]
 
     def layout(self):
         folder_dir = self.conf.get("user.path:folder_dir", default=None)
