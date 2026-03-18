300fdc93e8aa0606b5fd062972f95d4d3b482cde
Joshua Kim <52115847+dynamicduho@users.noreply.github.com>
2026-03-18 15:24:34 -0500

    update conan_mac to build nasm from source when prebuilt binary not found

--- build-scripts/mac/conan_mac.sh
+++ build-scripts/mac/conan_mac.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 rm -rf ./conan_x64
 rm -rf ./conan_arm
 
-conan install ./build-scripts/conanfile.py -u -pr:h ./build-scripts/mac/profile_mac14.0 -pr:b ./build-scripts/mac/profile_mac_armv8 -of ./conan_x64/
+conan install ./build-scripts/conanfile.py -u -pr:h ./build-scripts/mac/profile_mac14.0 -pr:b ./build-scripts/mac/profile_mac_armv8 --build=nasm* -of ./conan_x64/
 conan install ./build-scripts/conanfile.py -u -pr:h ./build-scripts/mac/profile_mac_armv8 -pr:b ./build-scripts/mac/profile_mac_armv8 -of ./conan_arm/
