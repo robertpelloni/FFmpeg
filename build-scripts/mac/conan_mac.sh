5c600cb3f8c9b491cad0968d907e45bb377b7376
Joshua Kim <52115847+dynamicduho@users.noreply.github.com>
2026-03-18 16:21:33 -0500

    fix: revert, don't build nasm from source, fixed remote nasm build

--- build-scripts/mac/conan_mac.sh
+++ build-scripts/mac/conan_mac.sh
@@ -4,5 +4,5 @@ set -e
 rm -rf ./conan_x64
 rm -rf ./conan_arm
 
-conan install ./build-scripts/conanfile.py -u -pr:h ./build-scripts/mac/profile_mac14.0 -pr:b ./build-scripts/mac/profile_mac_armv8 --build=nasm* -of ./conan_x64/
+conan install ./build-scripts/conanfile.py -u -pr:h ./build-scripts/mac/profile_mac14.0 -pr:b ./build-scripts/mac/profile_mac_armv8 -of ./conan_x64/
 conan install ./build-scripts/conanfile.py -u -pr:h ./build-scripts/mac/profile_mac_armv8 -pr:b ./build-scripts/mac/profile_mac_armv8 -of ./conan_arm/
