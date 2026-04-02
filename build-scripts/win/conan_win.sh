85f6f15e6d252c771f9c3092c9719b110e6bc0d9
Joshua Kim <52115847+dynamicduho@users.noreply.github.com>
2026-04-02 14:25:57 -0500

    use icx to compile conan libs

--- build-scripts/win/conan_win.sh
+++ build-scripts/win/conan_win.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+USE_ICX=false
+for arg in "$@"; do
+  case $arg in
+    --icx) USE_ICX=true; shift ;;
+    *) ;;
+  esac
+done
+
 rm -rf ./conan
 
-conan install ./build-scripts/conanfile.py -u -pr:b ./build-scripts/win/profile_win2022 -pr:h ./build-scripts/win/profile_win2022 -of ./conan
+if [ "$USE_ICX" = true ]; then
+  PROFILE="./build-scripts/win/profile_win2022_icx"
+  BUILD_FLAGS="--build=zimg --build=libvpx --build=libaom-av1"
+else
+  PROFILE="./build-scripts/win/profile_win2022"
+  BUILD_FLAGS=""
+fi
+
+conan install ./build-scripts/conanfile.py -u \
+  -pr:b ./build-scripts/win/profile_win2022 \
+  -pr:h "$PROFILE" \
+  -of ./conan \
+  $BUILD_FLAGS
